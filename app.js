832a72b890e410401818e29ba00c85f4689b6b87
atousley <annette.tousley@gmail.com>
2016-02-01 11:35:33 -0600

    turned in

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+	var totalSal = 0;
+
 	$('#empInfo').on('submit', function(event){
 		event.preventDefault();
 
@@ -7,7 +9,6 @@ $(document).ready(function(){
 		
 		$.each($('#empInfo').serializeArray(), function(i, field) {
 			results[field.name] = field.value;
-			// console.log(results);
 		});
 
 		$('#empInfo').find('input[type=text]').val('');
@@ -16,10 +17,20 @@ $(document).ready(function(){
 	});
 	
 	function appendDom(answers){
-		$('#container').append('<div></div>')
+		$('#container').append('<div></div>');
 		var $elements = $('#container').children().last();
+		totalSal = totalSal + parseInt(answers.annualSal / 12);
 
-		$elements.append('<p>' + answers.annualSal + '</p>');
+		$elements.append('<p>' + 'Full Name:' + ' ' + answers.fullName + '</p>');
+		$elements.append('<p>' + 'Id Number:' + ' ' + answers.idNum + '</p>');
+		$elements.append('<p>' + 'Job title:' + ' ' + answers.title + '</p>');
+		$elements.append('<p>' + 'Annual Salary:' + ' ' + answers.annualSal + '</p>');
+		$elements.append('<p>' + 'Monthly Salary Cost:' + ' ' + totalSal + '</p>');
 
+		// $('#container').find('span').val('indivSal');
 	}
+
+	$('#deleteButton').on('click', function(){
+		$('#container').children().last().remove();
+	});
 });
