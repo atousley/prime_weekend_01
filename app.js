2562f49bcffa3018d1fc30202737e8d6188b74fc
atousley <annette.tousley@gmail.com>
2016-02-01 11:52:06 -0600

    working pro mode

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
 	var totalSal = 0;
+	var empArray = [];
 
 	$('#empInfo').on('submit', function(event){
 		event.preventDefault();
@@ -11,26 +12,43 @@ $(document).ready(function(){
 			results[field.name] = field.value;
 		});
 
+		totalSal = totalSal + parseInt(results.annualSal / 12);
+		$('#total').replaceWith('<span id="total">' + '$' + totalSal + '</span>');
+		// Math.round(parseFloat()) this;
+
+		empArray.push(results, totalSal);
+
 		$('#empInfo').find('input[type=text]').val('');
+		$('#empInfo').focus('#empInfo.fullName');
 		appendDom(results);
-		
 	});
+
+	$('#container').on('click', '.deleteButton', function() {
+			var index = $(this).data().id;
+			var employee = empArray[index];
+
+			totalSal -= Math.round(employee.annualSal /12);
+			totalSal = parseFloat(totalSal);
+			updateSalary(totalSal);
+
+			$(this).parent().remove();
+	});
+
+	function updateSalary(salary) {
+		$('#total').text('$' + salary);
+	}
 	
 	function appendDom(answers){
 		$('#container').append('<div></div>');
 		var $elements = $('#container').children().last();
-		totalSal = totalSal + parseInt(answers.annualSal / 12);
 
 		$elements.append('<p>' + 'Full Name:' + ' ' + answers.fullName + '</p>');
 		$elements.append('<p>' + 'Id Number:' + ' ' + answers.idNum + '</p>');
 		$elements.append('<p>' + 'Job title:' + ' ' + answers.title + '</p>');
-		$elements.append('<p>' + 'Annual Salary:' + ' ' + answers.annualSal + '</p>');
-		$elements.append('<p>' + 'Monthly Salary Cost:' + ' ' + totalSal + '</p>');
-
-		// $('#container').find('span').val('indivSal');
+		$elements.append('<p>' + 'Annual Salary:' + ' $' + answers.annualSal + '</p>');
+		$elements.append('<button class="deleteButton" data-id="' + (empArray.length - 1) + '">Delete Employee</button>');	
 	}
 
-	$('#deleteButton').on('click', function(){
-		$('#container').children().last().remove();
-	});
 });
+
+//  id="employee' + answers.idNum + '"
